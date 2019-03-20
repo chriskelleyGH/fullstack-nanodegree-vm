4bc61ae69d44a43f949a998b3ef616e592d1d576
Chris Kelley <ckemail2@gmail.com>
2019-03-19 20:29:38 -0400

    Add additional functions

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -30,35 +30,62 @@ def newItem():
         categories = session.query(Category).all()
         return render_template('newitem.html', categories=categories)
 
+@app.route('/catalog/category/<int:category_id>/')
 @app.route('/catalog/category/<int:category_id>/items/')
 def showItems(category_id):
-    return 'This page will be for showing the items for each category'
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id = category_id).all()
+    return render_template('items.html', items=items, category=category)
 
 @app.route('/catalog/category/<int:category_id>/item/<int:item_id>/')
 def showItem(category_id, item_id):
-    return 'This page will be for showing item %s' % item_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    item = session.query(Item).filter_by(id=item_id).one()
+    return render_template ('item.html', item=item, category=category)
 
-@app.route('/catalog/category/<int:category_id>/item/<int:item_id>/edit/')
-def editItem(item_id):
-    return 'This page will be for editing item %s' % item_id
+@app.route('/catalog/category/<int:category_id>/item/<int:item_id>/edit/', methods=['GET', 'POST'])
+def editItem(category_id, item_id):
+    editedItem = session.query(Item).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        if request.form['description']:
+            editedItem.description = request.form['description']
+        if request.form['category']:
+            editedItem.category_id = request.form['category']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        categories = session.query(Category).all()
+        return render_template('edititem.html', category_id=category_id, item_id=item_id, item=editedItem, categories=categories)
 
-@app.route('/catalog/category/<int:category_id>/item/<int:item_id>/delete/')
-def deleteItem(item_id):
-    return 'This page will be for deleting item %s' % item_id
+@app.route('/catalog/category/<int:category_id>/item/<int:item_id>/delete/', methods=['GET', 'POST'])
+def deleteItem(category_id, item_id):
+    itemToDelete = session.query(Item).filter_by(id=item_id).one()
+    if request.method == 'POST':
+        session.delete(itemToDelete)
+        session.commit()
+        return redirect(url_for('showItems', category_id=category_id))
+    else:
+        return render_template('deleteitem.html', category_id=category_id, item_id=item_id, item=itemToDelete)
+
 
 @app.route('/catalog/JSON/')
 def catalogJSON():
     return 'This page will be for the catalog json endpoint'
 
+
 @app.route('/catalog/category/<int:category_id>/JSON/')
 def categoryJSON(category_id):
-    return 'This page will be for category %s json endpoint' % category_id
+    category = session.query(Category).filter_by(id=category_id).one()
+    items = session.query(Item).filter_by(category_id=category_id).all()
+    return jsonify(Items=[i.serialize for i in items])
 
 @app.route('/catalog/category/<int:category_id>/item/<int:item_id>/JSON/')
 def itemJSON(category_id, item_id):
-    return 'This page will be for item %s json endpoint' % item_id
-
-
+    Catalog_Item = session.query(Item).filter_by(id=item_id).one()
+    return jsonify(Catalog_Item=Catalog_Item.serialize)
 
 if __name__ == '__main__':
     app.debug = True
